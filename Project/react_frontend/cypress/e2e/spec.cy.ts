f9cb8ef515b64bb3eba54345b6b38d3717cda377
Shur <89518627+JosuelCastro@users.noreply.github.com>
2023-10-31 09:17:40 +0100

    Modified Cypress tests
    3/5 test work, other must still be modified

--- Project/react_frontend/cypress/e2e/spec.cy.ts
+++ Project/react_frontend/cypress/e2e/spec.cy.ts
@@ -1,9 +1,26 @@
+describe('Login', () => {
+  it('tries to log in as user', () => {
+    cy.visit('http://localhost:3000')
+
+    cy.get('.nav-link')
+        .eq(3)
+        .click()
+
+    cy.get('#email')
+        .type('micky.dai@gmail.com')
+
+    cy.get('#password')
+        .type('1234')
+
+    cy.get('.MuiButtonBase-root')
+        .click()
+  })
+})
+
 describe('Home', () => {
   it('checks if the Homepage is correct', () => {
     cy.visit('http://localhost:3000')
-
     cy.get('.nav-link').eq(0).click()
-
     cy.get('.MuiBox-root')
         .should('exist')
         .contains('Get Started')
@@ -26,21 +43,48 @@ describe('Home', () => {
   })
 })
 
-describe('Login', () => {
-  it('tries to log in as user', () => {
-    cy.visit('http://localhost:3000')
 
-    cy.get('.nav-link')
-        .eq(3)
-        .click()
+describe('User Registration', () => {
+  it('should register a new user', () => {
+    cy.visit('http://localhost:3000/register');
 
-    cy.get('#email')
-        .type('micky.dai@gmail.com')
+    cy.get('#username').type('newUser');
+    cy.get('#email').type('newuser@example.com');
+    cy.get('#password').type('password123');
+    cy.contains('button', 'Register').click();
 
-    cy.get('#password')
-        .type('1234')
+    // Add assertions to check if registration was successful
+  });
+});
 
-    cy.get('.MuiButtonBase-root')
-        .click()
-  })
-})
+describe('User Profile', () => {
+  it('should update user profile information', () => {
+    cy.visit('http://localhost:3000/login');
+
+    // Assuming user is logged in
+    cy.get('#email').type('user@example.com');
+    cy.get('#password').type('password123');
+    cy.get('.login-button').click();
+
+    cy.visit('http://localhost:3000/profile');
+    cy.get('#firstName').clear().type('UpdatedFirstName');
+    cy.get('#lastName').clear().type('UpdatedLastName');
+    cy.get('.save-profile-button').click();
+
+    // Add assertions to check if profile update was successful
+  });
+});
+
+describe('Navigation', () => {
+  it('should navigate to different pages', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('.nav-link-home').click();
+    cy.url().should('include', '/home');
+
+    cy.get('.nav-link-profile').click();
+    cy.url().should('include', '/profile');
+
+    // Add more navigation tests as needed
+  });
+});
