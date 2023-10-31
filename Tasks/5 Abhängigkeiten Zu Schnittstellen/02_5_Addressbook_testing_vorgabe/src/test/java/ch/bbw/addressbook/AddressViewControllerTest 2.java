5233aaed48bc12d396d971de2c7f0ca423557cc6
Shur <89518627+JosuelCastro@users.noreply.github.com>
2023-10-31 08:20:15 +0100

    Update

---
+++ Tasks/5 Abhängigkeiten Zu Schnittstellen/02_5_Addressbook_testing_vorgabe/src/test/java/ch/bbw/addressbook/AddressViewControllerTest 2.java
@@ -0,0 +1,45 @@
+package ch.bbw.addressbook;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class AddressViewControllerTest {
+
+    private AddressViewController addressViewController;
+    private AddressService addressService;
+
+    @BeforeEach
+    public void setUp() {
+        addressService = Mockito.mock(AddressService.class);
+        addressViewController = new AddressViewController();
+    }
+
+    @Test
+    public void testGetAddresses() {
+        Address address1 = new Address(1, "John", "Doe", "123456789", new Date());
+        Address address2 = new Address(2, "Jane", "Smith", "987654321", new Date());
+        when(addressService.getAllAddresses()).thenReturn(Arrays.asList(address1, address2));
+
+        addressViewController.getAddresses();
+        List<Address> addresses = addressViewController.getAddresses();
+        assertNotNull(addresses);
+        assertEquals(2, addresses.size());
+    }
+
+    @Test
+    public void testSaveAddress() {
+        addressViewController.setFirstname("John");
+        addressViewController.setLastname("Doe");
+        addressViewController.saveAddress();
+
+        verify(addressService, times(1)).registerAddress(any(Address.class));
+    }
+}
